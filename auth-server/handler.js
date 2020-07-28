bb22dcc215d227fde24c1ac9e4216301606b182a
Stephanierlandry <stephanierlandry@gmail.com>
2020-07-28 14:36:18 -0500

    axios added. getAccessToken() defined

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,18 +1,23 @@
 'use strict';
 
-module.exports.hello = async event => {
+const axios = require('axios');
+
+module.exports.getAccessToken = async (event) => {
+
+  const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
+    + '?client_id=o3q8bebq46en36kbff39jm0srr'
+    + '&client_secret=r1n92h2a46tcrlhg1qr2u50150'
+    + '&grant_type=authorization_code'
+    + '&redirect_uri=https://stephanierlandry.github.io/meetup/'
+    + '&code=c4225085fda6772984291692ea27879b';
+
+  const info = await axios.post(MEETUP_OAUTH_URL);
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify({
+      access_token: info.data.access_token,
+      refresh_token: info.data.refresh_token,
+    }),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
