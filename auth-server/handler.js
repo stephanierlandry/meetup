de7c5577fa3765e291bd1ad61fdfad1d3478504a
Stephanierlandry <stephanierlandry@gmail.com>
2020-07-28 14:42:27 -0500

    will send a new oauth code for each user

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -9,7 +9,7 @@ module.exports.getAccessToken = async (event) => {
     + '&client_secret=r1n92h2a46tcrlhg1qr2u50150'
     + '&grant_type=authorization_code'
     + '&redirect_uri=https://stephanierlandry.github.io/meetup/'
-    + '&code=c4225085fda6772984291692ea27879b';
+    + '&code=' + event.pathParameters.code;
 
   const info = await axios.post(MEETUP_OAUTH_URL);
 
