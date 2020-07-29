45bfe9c6d712df9d1cf10368e1303b9ba6fdd8ca
Stephanierlandry <stephanierlandry@gmail.com>
2020-07-28 19:42:18 -0500

    console message removed

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -4,9 +4,6 @@ const axios = require('axios');
 
 module.exports.getAccessToken = async (event) => {
 
-
-  console.log('event.pathParameters.code')
-
   const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
     + '?client_id=o3q8bebq46en36kbff39jm0srr'
     + '&client_secret=r1n92h2a46tcrlhg1qr2u50150'
