5b83578083322d13523164bbd8217fd6611ff31f
Stephanierlandry <stephanierlandry@gmail.com>
2020-07-29 21:28:10 -0500

    getAccessToken () working

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -4,8 +4,6 @@ const axios = require('axios');
 
 module.exports.getAccessToken = async (event) => {
 
-  console.log('event', event);
-
   const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
     + '?client_id=o3q8bebq46en36kbff39jm0srr'
     + '&client_secret=r1n92h2a46tcrlhg1qr2u50150'
@@ -17,9 +15,35 @@ module.exports.getAccessToken = async (event) => {
 
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
     body: JSON.stringify({
       access_token: info.data.access_token,
       refresh_token: info.data.refresh_token,
     })
+  };
+};
+
+module.exports.refreshAccessToken = async (event) => {
+
+  const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
+    + '?client_id=o3q8bebq46en36kbff39jm0srr'
+    + '&client_secret=r1n92h2a46tcrlhg1qr2u50150'
+    + '&grant_type=refresh_token'
+    + '&refresh_token=' + event.pathParameters.code;
+
+  const info = await axios.post(MEETUP_OAUTH_URL)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify({
+      access_token: info.data.access_token,
+      refresh_token: info.data.refresh_token,
+    }),
   };
 };
