b779bb4c15d0eca0c23cad20d5ec24c4cade5c61
Stephanierlandry <stephanierlandry@gmail.com>
2020-07-30 17:57:51 -0500

    first test written

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,5 +3,8 @@ import { shallow } from 'enzyme';
 import App from '../App';
 
 describe('<App /> component', () => {
-
+  test('render list of events', () => {
+    const AppWrapper = shallow(<App />);
+    expect(AppWrapper.find(EventList)).toHaveLength(1);
+  });
 });
