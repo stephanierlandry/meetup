1f82c1fac4df03ac09c7b2f39dab315efd9c5cd9
Stephanierlandry <stephanierlandry@gmail.com>
2020-07-31 17:33:20 -0500

    EventList created. EventList added to App Component. EventList imported to test file

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+
 import App from '../App';
+import EventList from '../EventList';
 
 describe('<App /> component', () => {
   test('render list of events', () => {
