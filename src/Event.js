70532932834697766777620a0a44e1031a609b63
Stephanierlandry <stephanierlandry@gmail.com>
2020-07-31 14:40:25 -0500

    show/hide details button created and tested. event details created and tested

--- src/Event.js
+++ src/Event.js
@@ -2,10 +2,44 @@ import React, { Component } from 'react';
 
 class Event extends Component {
 
+  state = {
+    showDetails: false
+  }
+
+  handleShowDetails = () => {
+    if(this.state.showDetails === false) {
+      this.setState({ showDetails: true });
+    }
+    else {
+      this.setState({ showDetails: false });
+    }
+  }
+
   render() {
+    const showDetails = this.state.showDetails;
 
     return (
-      <div></div>
+      <div className="event">
+        <div className="eventOverview">
+          <h1 className="eventName"></h1>
+          <p className="eventDate"></p>
+          <p className="eventTime"></p>
+          <p className="eventVenue"></p>
+
+          {showDetails &&
+            <button className="details-btn" onClick={() => this.handleShowDetails()}>hide details</button>
+          }
+          {!showDetails &&
+            <button className="details-btn" onClick={() => this.handleShowDetails()}>show details</button>
+          }
+        </div>
+        {showDetails &&
+          <div className="eventDetails">
+            <p className="eventDescription"></p>
+            <p className="eventLink"></p>
+          </div>
+        }
+      </div>
     );
   }
 }
