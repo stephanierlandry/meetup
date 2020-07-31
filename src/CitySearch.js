60ad987ecd0115b4055337a51543f00737015289
Stephanierlandry <stephanierlandry@gmail.com>
2020-07-31 11:00:30 -0500

    CitySearch app lists suggestions based on city typed into the input. Events can be filted by city

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 
 class CitySearch extends Component {
+
+  state = {
+    query: '',
+    suggestions: []
+  }
+
+  handleInputChanged = (event) => {
+   const value = event.target.value;
+   this.setState({ query: value });
+  }
+
+  handleItemClicked = (value) => {
+    this.setState({ query: value });
+  }
+
   render() {
     return (
-      <div></div>
+      <div className="CitySearch">
+        <input type="text" className="city" value={this.state.query} onChange={this.handleInputChanged}/>
+        <ul className="suggestions">
+          {this.state.suggestions.map(item =>
+            <li key={item.name_string} onClick={() => this.handleItemClicked(item.name_string)}>{item.name_string}</li>
+          )}
+        </ul>
+      </div>
     );
   }
 }
