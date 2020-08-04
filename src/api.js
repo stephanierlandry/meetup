d676f8154d741b0e55b1bb92fe054c3108c6296d
Stephanierlandry <stephanierlandry@gmail.com>
2020-08-04 14:25:36 -0500

    getting or renewing access token based on user's local storage

--- src/api.js
+++ src/api.js
@@ -53,4 +53,28 @@ function getAccessToken() {
   return getOrRenewAccessToken('renew', refreshToken);
 };
 
+async function getOrRenewAccessToken(type, key) {
+  let url;
+  if (type === 'get') {
+    // Lambda endpoint to get token by code
+    url = 'https://dqcf31qq59.execute-api.eu-central-1.amazonaws.com/dev/api/token/'
+      + key;
+  } else if (type === 'renew') {
+    // Lambda endpoint to get token by refresh_token
+    url = 'https://dqcf31qq59.execute-api.eu-central-1.amazonaws.com/dev/api/refresh/'
+      + key;
+  }
+
+  // Use Axios to make a GET request to the endpoint
+  const tokenInfo = await axios.get(url);
+
+  // Save tokens to localStorage together with a timestamp
+  localStorage.setItem('access_token', tokenInfo.data.access_token);
+  localStorage.setItem('refresh_token', tokenInfo.data.refresh_token);
+  localStorage.setItem('last_saved_time', Date.now());
+
+  // Return the access_token
+  return tokenInfo.data.access_token;
+}
+
 export { getSuggestions, getEvents, getAccessToken };
