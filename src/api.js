22966bb84688dc77a8942b50eee391d2ebe14cdf
Stephanierlandry <stephanierlandry@gmail.com>
2020-08-04 14:25:05 -0500

    Getting access token

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 import { mockEvents } from './mockEvents/mockEvents.js';
+import axios from 'axios';
 
 async function getSuggestions(query) {
   return [
@@ -28,4 +29,28 @@ async function getEvents(lat, lon) {
   return mockEvents.events;
 }
 
-export { getSuggestions, getEvents };
+function getAccessToken() {
+  const accessToken = localStorage.getItem('access_token');
+
+  if (!accessToken) {
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get('code');
+
+    if (!code) {
+      window.location.href = 'https://secure.meetup.com/oauth2/authorize?client_id=o3q8bebq46en36kbff39jm0srr&response_type=code&redirect_uri=https://stephanierlandry.github.io/meetup/';
+      return null;
+    }
+    return getOrRenewAccessToken('get', code);
+  };
+
+  const lastSavedTime = localStorage.getItem('last_saved_time');
+
+  if (accessToken && (Date.now() - lastSavedTime < 3600000)) {
+    return accessToken;
+  }
+
+  const refreshToken = localStorage.getItem('refresh_token');
+  return getOrRenewAccessToken('renew', refreshToken);
+};
+
+export { getSuggestions, getEvents, getAccessToken };
