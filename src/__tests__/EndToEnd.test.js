139a1d8b77f4739680e51e0b3a05db5c76d69f1d
Stephanierlandry <stephanierlandry@gmail.com>
2020-08-10 13:05:56 -0500

    event element closed by default

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,4 +1,15 @@
 import puppeteer from 'puppeteer';
 
 describe('show/hide an event details', () => {
+  test('An event element is collapsed by default', async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+
+    await page.waitForSelector('.event');
+
+    const extra = await page.$('.event .extra');
+    expect(extra).toBeNull();
+    browser.close();
+  })
 });
