e14f8397c8a782a5965bd7f4c4ae02ae06b554a9
Stephanierlandry <stephanierlandry@gmail.com>
2020-08-10 15:19:36 -0500

    user can collapse event details

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -16,7 +16,6 @@ describe('show/hide an event details', () => {
     browser.close();
   })
 
-
   test('An event element is collapsed by default', async () => {
     const extra = await page.$('.event .extra');
     expect(extra).toBeNull();
@@ -26,6 +25,11 @@ describe('show/hide an event details', () => {
    await page.click('.event .detail-btn');
    const extra = await page.$('.event .extra');
    expect(extra).toBeDefined();
-   browser.close();
  });
+
+  test('User can collapse an events details', async () => {
+    await page.click('.event .detail-btn');
+    const extra = await page.$('.event .extra');
+    expect(extra).toBeNull();
+  })
 });
