8cf02b9ad991ae9de69c6d0be0cbf99a5c8eff2c
Stephanierlandry <stephanierlandry@gmail.com>
2020-08-10 14:50:14 -0500

    detail button shows details

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -12,4 +12,17 @@ describe('show/hide an event details', () => {
     expect(extra).toBeNull();
     browser.close();
   })
+
+  test('User can expand an event to see its details', async () => {
+   const browser = await puppeteer.launch();
+   const page = await browser.newPage();
+   await page.goto('http://localhost:3000/');
+
+   await page.waitForSelector('.event');
+   await page.click('.event .detail-btn');
+
+   const extra = await page.$('.event .extra');
+   expect(extra).toBeDefined();
+   browser.close();
+ });
 });
