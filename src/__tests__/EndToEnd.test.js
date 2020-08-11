20d65ee9e60608887e37bb7e26e1a2684668102b
Stephanierlandry <stephanierlandry@gmail.com>
2020-08-10 21:53:43 -0500

    filter event by city test written

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -32,5 +32,37 @@ describe('show/hide an event details', () => {
     const extra = await page.$('.event .extra');
     expect(extra).toBeNull();
   })
-  
+});
+
+describe('filter event by city', () => {
+
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event');
+  });
+
+  afterAll(() => {
+    browser.close();
+  })
+
+  test('show upcoming events based on user location', async () => {
+    const events = await page.$('.event');
+    expect(events).toBeDefined();
+  })
+
+  test('user should receive a match of suggestions matching what they type', async () => {
+    const suggestion = await page.$('.suggestions li');
+    expect(suggestion).toBeDefined();
+  })
+
+  test('user can select city from suggestion list', async () => {
+    expect('.suggestion li').toBeDefined();
+    expect('.city').toBeDefined();
+  })
+
 });
